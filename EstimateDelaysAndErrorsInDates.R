d1211746ea24d5f0f280f8c9a4f1b9197407e394
Anne Cori <a.cori@imperial.ac.uk>
2016-11-18 12:07:26 +0000

    adding to to do list at end of code

--- EstimateDelaysAndErrorsInDates.R
+++ EstimateDelaysAndErrorsInDates.R
@@ -516,7 +516,7 @@ move_truncated_lognormal <- function(what=c("zeta"), sdlog, upper_bound=1,
 
 # Anne: 
 # in the move functions, only update relevant bits of the likelihood to speed up
-# 
+# write the MCMC
 
 # Marc: 
 # finish writing

e19fea425647a7927c1107d57ded8581272c66e6
Anne Cori <a.cori@imperial.ac.uk>
2016-11-22 11:56:29 +0000

    adding new file to simulate data

---
+++ Simulator.R
@@ -0,0 +1,62 @@
+#######################################
+### functions to simulate a dataset ###
+#######################################
+
+find_params_gamma <- function(mu, sigma)
+{
+  a <- ((mu - 1)/sigma)^2
+  b <- sigma^2/(mu - 1)
+  return(c(a, b))
+}
+
+simul_true_data <- function(theta, n_per_group, range_dates, index_dates)
+{
+  D <- list()
+  for(g in 1:length(theta$mu))
+  {
+    D[[g]] <- matrix(NA, n_per_group[g], length(theta$mu[[g]])+1)
+    D[[g]][,1] <- sample(range_dates[1]:range_dates[2], n_per_group[g], replace = TRUE)
+    for(j in 1:ncol(index_dates[[g]]))
+    {
+      params <- find_params_gamma(theta$mu[[g]], theta$sigma[[g]])
+      delay <- rgamma(n_per_group[g], shape=params[1], scale=params[2])
+      D[[g]][,index_dates[[g]][2,j]]  <- D[[g]][,index_dates[[g]][1,j]] + round(delay)
+    }
+  }
+  return(D)
+}
+
+simul_obs_dat <- function(D, theta)
+{
+  E <- D
+  obs_dat <- D
+  for(g in 1:length(D))
+  {
+    for(j in 1:ncol(D[[g]]))
+    {
+      E[[g]][,j] <- sample(c(-1, 1, 0), nrow(D[[g]]), replace=TRUE, prob=c(theta$prop_missing_data, (1-theta$prop_missing_data)*theta$zeta, (1-theta$prop_missing_data)*(1-theta$zeta)))
+      obs_dat[[g]][E[[g]][,j]==-1,j]  <- NA
+      obs_dat[[g]][E[[g]][,j]==0,j]  <- D[[g]][E[[g]][,j]==0,j]
+      obs_dat[[g]][E[[g]][,j]==1,j]  <- sample(range_dates[1]:range_dates[2], sum(E[[g]][,j]==1), replace = TRUE) # need to update if change error model
+    }
+  }
+  return(list(obs_dat=obs_dat, E=E))
+}
+
+####################################
+### creating a simulated dataset ###
+####################################
+
+theta$prop_missing_data <- 0.1 ### this is currently missing from the estimation model
+theta$zeta <- 0.1 ### probability that, when not missing, the date is recorded with error
+n_per_group <- rep(100, 4)
+D <- simul_true_data(theta, n_per_group, range_dates, index_dates)
+tmp <- simul_obs_dat(D, theta)
+E <- tmp$E
+obs_dat <- tmp$obs_dat
+
+#########################################
+### applying the MCMC to this dataset ###
+#########################################
+
+# TO WRITE

6e823168fff3ce834bd76c958ac1099d912a1940
Anne Cori <a.cori@imperial.ac.uk>
2016-11-23 17:27:30 +0000

    updating SimulateDelaysAndErrorsInDates.R to use mu, CV instead of mu, sigma

--- SimulateDelaysAndErrorsInDates.R
+++ SimulateDelaysAndErrorsInDates.R
@@ -18,7 +18,7 @@ theta <- list()
 theta$prop_missing_data <- 0.1 ### this is missing from the estimation model (directly available from the data so not explicitely modelled)
 theta$zeta <- 0.1 ### probability that, when not missing, the date is recorded with error
 theta$mu <- list(5, c(6, 7), c(8, 9, 10), c(11, 12, 13))
-theta$sigma <- list(3, c(3, 3), c(3, 3, 3), c(3, 3, 3))
+theta$CV <- list(0.5, c(0.5, 0.5), c(0.5, 0.5, 0.5), c(0.5, 0.5, 0.5))
 n_per_group <- rep(100, 4)
 range_dates <- date_to_int(c(as.Date("01/01/2014", "%d/%m/%Y"), as.Date("01/01/2015", "%d/%m/%Y")))
 index_dates <- list(matrix(c(1, 2), nrow=2), cbind(c(1, 2), c(1, 3)), cbind(c(1, 2), c(2, 3), c(1, 4)), cbind(c(1, 2), c(2, 3), c(1, 4)) )

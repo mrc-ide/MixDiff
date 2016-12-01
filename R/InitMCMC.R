fcd1c9e9000f88914adfed5662bdc48a3039c85d
Anne Cori <a.cori@imperial.ac.uk>
2016-12-01 12:01:01 +0000

    changed 1:n to seq_len(n) in InitMCMC.R

--- R/InitMCMC.R
+++ R/InitMCMC.R
@@ -62,15 +62,15 @@ initialise_aug_data <- function(obs_dat, index_dates_order, MCMC_settings)
 {
   n_groups <- length(obs_dat)
   D <- list()
-  for(g in 1:n_groups) 
+  for(g in seq_len(n_groups) )
   {
     D[[g]] <- obs_dat[[g]]
-    for(e in 1:nrow(D[[g]]))
+    for(e in seq_len(nrow(D[[g]])))
     {
       #print(e)
       
       # first deal with incompatible dates
-      for(j in 1:ncol(index_dates_order[[g]]))
+      for(j in seq_len(ncol(index_dates_order[[g]])))
       {
         if(!any(is.na(D[[g]][e,index_dates_order[[g]][,j]])))
         {
@@ -78,7 +78,7 @@ initialise_aug_data <- function(obs_dat, index_dates_order, MCMC_settings)
           if(are_dates_incompatible(D[[g]][e,index_dates_order[[g]][1,j]], D[[g]][e,index_dates_order[[g]][2,j]], MCMC_settings$init_options$mindelay, MCMC_settings$init_options$maxdelay) )
           {
             # check if there is one of the dates involved in more than one problematic delays, if so must be the problematic one:
-            tmp <- table(as.vector(index_dates_order[[g]][,sapply(1:ncol(index_dates_order[[g]]), function(j) are_dates_incompatible(D[[g]][e,index_dates_order[[g]][1,j]], D[[g]][e,index_dates_order[[g]][2,j]], MCMC_settings$init_options$mindelay, MCMC_settings$init_options$maxdelay) )]))
+            tmp <- table(as.vector(index_dates_order[[g]][,sapply(seq_len(ncol(index_dates_order[[g]])), function(j) are_dates_incompatible(D[[g]][e,index_dates_order[[g]][1,j]], D[[g]][e,index_dates_order[[g]][2,j]], MCMC_settings$init_options$mindelay, MCMC_settings$init_options$maxdelay) )]))
             if(any(tmp>1))
             {
               must_be_wrong <- which.max(tmp)[1]
@@ -93,7 +93,7 @@ initialise_aug_data <- function(obs_dat, index_dates_order, MCMC_settings)
             while(!(must_be_wrong %in% index_dates_order[[g]][,j]))
             {
               # check if there is one of the dates involved in more than one problematic delays, if so must be the problematic one:
-              tmp <- table(as.vector(index_dates_order[[g]][,sapply(1:ncol(index_dates_order[[g]]), function(j) are_dates_incompatible(D[[g]][e,index_dates_order[[g]][1,j]], D[[g]][e,index_dates_order[[g]][2,j]], MCMC_settings$init_options$mindelay, MCMC_settings$init_options$maxdelay) )]))
+              tmp <- table(as.vector(index_dates_order[[g]][,sapply(seq_len(ncol(index_dates_order[[g]])), function(j) are_dates_incompatible(D[[g]][e,index_dates_order[[g]][1,j]], D[[g]][e,index_dates_order[[g]][2,j]], MCMC_settings$init_options$mindelay, MCMC_settings$init_options$maxdelay) )]))
               if(any(tmp>1))
               {
                 must_be_wrong <- which.max(tmp)[1]
@@ -116,12 +116,12 @@ initialise_aug_data <- function(obs_dat, index_dates_order, MCMC_settings)
       {
         can_be_inferred_from <- lapply(missing_dates, function(i) {
           x <- which(index_dates_order[[g]]==i, arr.ind = TRUE)
-          from_idx <- sapply(1:nrow(x), function(k) index_dates_order[[g]][-x[k,1],x[k,2]] )
-          from_value <- sapply(1:nrow(x), function(k) D[[g]][e,index_dates_order[[g]][-x[k,1],x[k,2]]])
-          rule <- sapply(1:nrow(x), function(k) if(x[k,1]==1) "before" else "after"  )
+          from_idx <- sapply(seq_len(nrow(x)), function(k) index_dates_order[[g]][-x[k,1],x[k,2]] )
+          from_value <- sapply(seq_len(nrow(x)), function(k) D[[g]][e,index_dates_order[[g]][-x[k,1],x[k,2]]])
+          rule <- sapply(seq_len(nrow(x)), function(k) if(x[k,1]==1) "before" else "after"  )
           return(list(rule=rule,from_idx=from_idx, from_value=from_value))
         })
-        can_be_inferred <- which(sapply(1:length(missing_dates), function(i) any(!is.na(can_be_inferred_from[[i]]$from_value))))
+        can_be_inferred <- which(sapply(seq_len(length(missing_dates)), function(i) any(!is.na(can_be_inferred_from[[i]]$from_value))))
         for(k in can_be_inferred)
         {
           x <- which(!is.na(can_be_inferred_from[[k]]$from_value))
@@ -159,10 +159,10 @@ initialise_aug_data <- function(obs_dat, index_dates_order, MCMC_settings)
   
   # compute E accordingly
   E <- list()
-  for(g in 1:n_groups) 
+  for(g in seq_len(n_groups) )
   {
     E[[g]] <- matrix(NA,nrow(obs_dat[[g]]),ncol(obs_dat[[g]]))
-    for(j in 1:ncol(obs_dat[[g]]))
+    for(j in seq_len(ncol(obs_dat[[g]])) )
     {
       E[[g]][!(D[[g]][,j] %in% obs_dat[[g]][,j]),j] <- 1 # error
       E[[g]][D[[g]][,j] %in% obs_dat[[g]][,j],j] <- 0 # no error
@@ -229,11 +229,11 @@ initialise_theta_from_aug_dat <- function(aug_dat, index_dates, zeta_init=0.1) #
   ### mean and std of distribution of various delays, by group
   ### we use a the starting point the observed mean and std of each delay in each group
   obs_delta <- compute_delta(aug_dat$D, index_dates)
-  mu <- lapply(1:n_groups, function(g) abs(apply(obs_delta[[g]], 2, mean, na.rm=TRUE) ))
+  mu <- lapply(seq_len(n_groups), function(g) abs(apply(obs_delta[[g]], 2, mean, na.rm=TRUE) ))
   names(mu) <- names(n_dates)
-  sigma <- lapply(1:n_groups, function(g) abs(apply(obs_delta[[g]], 2, sd, na.rm=TRUE) ))
+  sigma <- lapply(seq_len(n_groups), function(g) abs(apply(obs_delta[[g]], 2, sd, na.rm=TRUE) ))
   names(sigma) <- names(n_dates)
-  CV <- lapply(1:n_groups, function(g) sigma[[g]]/mu[[g]])
+  CV <- lapply(seq_len(n_groups), function(g) sigma[[g]]/mu[[g]])
   names(CV) <- names(n_dates)
   
   ### list of all parameters
